a2a2f9f4db507b0b83db6abc07023fd3069f119e
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-05-13 20:47:00 +0530

    database connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
-const app =express()
+dotenv.config();
 
-app.listen(5050,()=>{
-    console.log('Server isd running on port 5050')
-})
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log('Database connected');
+  })
+  .catch((error) => {
+    console.error('Database connection error:', error);
+  });
+
+const app = express();
+
+app.listen(5050, () => {
+  console.log('Server is running on port 5050');
+});
