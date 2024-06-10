9f9f4861be6e4ad343a05edaccfb97e512df432a
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-06-10 16:55:38 +0530

    header updated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,16 @@
-import { Button, Navbar, TextInput } from 'flowbite-react'
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai'
 import {FaMoon} from 'react-icons/fa'
+import { useSelector } from 'react-redux'
 
 
 
 function Header() {
 
     const path =useLocation().pathname
+    const {currentUser} = useSelector(state=> state.user)
 
 
   return (
@@ -33,11 +35,41 @@ function Header() {
             <Button className='w-12 h-10 hidden sm:inline ' color='grey' pill>
                 <FaMoon/>
             </Button>
-            <Link to='/sign-in'>
+            {currentUser ? (
+                <Dropdown arrowIcon={false} inline label={
+                    <Avatar alt='user' img={currentUser.profilePicture} rounded />
+                }>
+                <Dropdown.Header>
+                    <span className='block text-sm' >
+                        @{currentUser.username}
+                    </span>
+                    <span className='block text-sm font-medium truncate' >
+                        {currentUser.email}
+                    </span>
+                </Dropdown.Header>
+                <Link to={'/dashboard?tab=profile'}>
+                    <Dropdown.Item>
+                        Profile
+                    </Dropdown.Item>
+                </Link>
+                <Dropdown.Divider/>
+                <Link>
+                    <Dropdown.Item>
+                        Sign Out
+                    </Dropdown.Item>
+
+                </Link>
+
+                
+           </Dropdown>
+            ) :(
+                <Link to='/sign-in'>
                 <Button gradientDuoTone='purpleToBlue' outline>
                     Sign In
                 </Button>
             </Link>
+            )}
+            
             <Navbar.Toggle/>
         </div>
         <Navbar.Collapse>
