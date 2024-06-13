6e17d145fa89032608d329dd87e52183c637f47d
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-06-13 19:33:59 +0530

    create get users api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,7 +2,7 @@ import express from 'express';
 import {
   deleteUser,
 //   getUser,
-//   getUsers,
+  getUsers,
   signout,
   test,
   updateUser,
@@ -15,7 +15,7 @@ router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
-// router.get('/getusers', verifyToken, getUsers);
+router.get('/getusers', verifyToken, getUsers);
 // router.get('/:userId', getUser);
 
 export default router;
