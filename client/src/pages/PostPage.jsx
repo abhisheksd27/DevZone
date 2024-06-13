ea2777e71667d553bc49b1abac4a00b06dae6047
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-06-13 20:16:27 +0530

    add call to action

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,7 +1,7 @@
 import { Button, Spinner } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-// import CallToAction from '../components/CallToAction';
+import CallToAction from '../components/CallToAction';
 // import CommentSection from '../components/CommentSection';
 // import PostCard from '../components/PostCard';
 
@@ -83,10 +83,10 @@ export default function PostPage() {
       </div>
       <div className='p-3 max-w-2xl mx-auto w-full post-content'
         dangerouslySetInnerHTML={{ __html: post && post.content }}>
-            
+
         </div>
       <div className='max-w-4xl mx-auto w-full'>
-        {/* <CallToAction /> */}
+        <CallToAction />
       </div>
       {/* <CommentSection postId={post._id} /> */}
 
