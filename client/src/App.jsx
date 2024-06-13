e9bf37d3e49760fcd7c7ae616aa2b27a551ce939
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-06-13 17:38:52 +0530

    update posts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import FooterComponent from './components/FooterComponent';
 import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePost';
 
 
 
@@ -30,7 +31,9 @@ const App = () => {
         </Route>
         <Route element={<OnlyAdminPrivateRoute/>}>
           <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:postId" element={<UpdatePost />} />
         </Route>
+        
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <FooterComponent/>
