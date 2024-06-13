42606d5519158cf8f73f09bd281b0c0555de4b34
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-06-13 16:54:50 +0530

    all about posts

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,17 +1,17 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
 import { create,
-    //  deletepost, 
+     deletepost, 
      getposts, 
-    //  updatepost 
+     updatepost 
     } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create)
 router.get('/getposts', getposts)
-// router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
-// router.put('/updatepost/:postId/:userId', verifyToken, updatepost)
+router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
+router.put('/updatepost/:postId/:userId', verifyToken, updatepost)
 
 
 export default router;
