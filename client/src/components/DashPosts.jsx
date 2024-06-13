52e0e5250c8b89ebbf717b597ae2475cedc48b86
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-06-13 16:13:36 +0530

    add posts section to the dashboard

---
+++ client/src/components/DashPosts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashPosts = () => {
+  return (
+    <div>
+      DashPosts
+    </div>
+  )
+}
+
+export default DashPosts
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,35 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
-import DashProfile from '../components/DashProfile'
-import DashSidebar from '../components/DashSidebar'
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
+import DashPosts from '../components/DashPosts';
+// import DashUsers from '../components/DashUsers';
+// import DashComments from '../components/DashComments';
+// import DashboardComp from '../components/DashboardComp';
 
-const Dashboard = () => {
-  const location = useLocation()
-  const [tab, setTab] = useState('')
+export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
   useEffect(() => {
-    const urlParams = new URLSearchParams(location.search)
-    const tabFromUrl = urlParams.get('tab')
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
     if (tabFromUrl) {
-      setTab(tabFromUrl)
+      setTab(tabFromUrl);
     }
-  }, [location.search])
-
+  }, [location.search]);
   return (
     <div className='min-h-screen flex flex-col md:flex-row'>
       <div className='md:w-56'>
-        {/* sidebar */}
+        {/* Sidebar */}
         <DashSidebar />
       </div>
-      <div className='flex-grow flex justify-center'>
-        {/* profile */}
-        {tab === 'profile' && (
-          <div className='max-w-lg mx-auto w-full'>
-            <DashProfile />
-          </div>
-        )}
-      </div>
+      {/* profile... */}
+      {tab === 'profile' && <DashProfile />}
+      {/* posts... */}
+      {tab === 'posts' && <DashPosts />}
+      {/* users */}
+      {/* {tab === 'users' && <DashUsers />} */}
+      {/* comments  */}
+      {/* {tab === 'comments' && <DashComments />} */}
+      {/* dashboard comp */}
+      {/* {tab === 'dash' && <DashboardComp />} */}
     </div>
-  )
+  );
 }
-
-export default Dashboard
