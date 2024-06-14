d28bf5d197fcd07f23933b30635082d18ce0ea5f
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-06-14 14:28:41 +0530

    deploy changes

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth.route.js'
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path'
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGO)
     console.error('Database connection error:', error);
   });
 
+  const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -35,6 +37,14 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, 'client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client','dist', 'index.html'));
+});
+
+
 
 
 
