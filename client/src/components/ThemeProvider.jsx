70833bc70af7d31bf4ded181858303f1140310ca
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-06-20 11:51:12 +0530

    theme

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -6,10 +6,10 @@ function ThemeProvider({ children }) {
 
     return (
         <div className={theme}>
-            <div className={`bg-white text-black-900 dark:text-white-200 dark:bg-[rgb(16,23,43)] min-h-screen`}>
-                {children}
-            </div>
+        <div className='bg-white text-gray-700 dark:text-gray-200 dark:bg-[rgb(16,23,42)] min-h-screen'>
+          {children}
         </div>
+      </div>
     );
 }
 
